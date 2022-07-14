7225630ee57cedc44927a8bcf9c680128a2452f2
Andrzej Mironowicz <andrzej.mironowicz@gmail.com>
2022-07-14 13:04:21 +0200

    test

--- Package.swift
+++ Package.swift
@@ -5,12 +5,16 @@ let package = Package(
     name: "PiwikPROSDK",
     platforms: [.iOS(.v9)],
     products: [
-        .library(name: "PiwikPROSDK", targets: ["PiwikPROSDK"]),
+        .library(
+            name: "PiwikPROSDK",
+            targets: ["PiwikPROSDK"]),
     ],
     dependencies: [
         
     ],
     targets: [
-        .binaryTarget(name: "PiwikPROSDK", path: "./PiwikPROSDK.xcframework")
+        .binaryTarget(name: "PiwikPROSDK",
+                      url: "https://github.com/mandrzej/test3/releases/download/1.0.0/PiwikPROSDK.xcframework.zip",
+                      checksum: "77a3be038b3f6d864c0762ccaf64bf82b7f4ac5e6fb9c04456eeaaf76bd07c90"),
     ]
 )
